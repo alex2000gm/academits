13286381f0a37257dc29220fa3843ebce0e9913b
alex2000gm <alex2000gm@gmail.com>
2016-09-10 00:26:17 +0700

    Добавил кнопку удалить. Работает с помощью чекбоксов.

--- public_html/phoneBook/phonebook.js
+++ public_html/phoneBook/phonebook.js
@@ -6,17 +6,17 @@ $(document).ready(function () {
             var str = "";
             $(".emptyField").append(str.concat("<td>", $(this).val(), "</td>"));
         });
-        $(".emptyField").append("<td><button id=\"removeContact\"><img src=\"phonebook_files/remove-button.png\" alt=\"удалить\"></button></td>");
+        $(".emptyField").append("<td ><input type=\"checkbox\"/></td>");
+        //$(".emptyField").append("<td><button class=\"removeContact\"><img src=\"phonebook_files/remove-button.png\" alt=\"удалить\"></button></td>");
         $("tr").removeClass("emptyField");
 
         getQueueNum();
 
     });
     $("#removeContact").click(function () {
-        $(("nearest").parents()).remove();
+        $(":checked").closest("tr").remove();
         getQueueNum();
     });
-
 
     function getQueueNum() {
         var queueNumField = $(".queueNum");
